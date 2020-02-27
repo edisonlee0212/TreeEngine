07ac55de5585f4e05876be8c1e21c3c87e047de2
Edison Li <li2343@purdue.edu>
2020-02-26 21:48:42 -0500

    fix

--- main.cpp
+++ main.cpp
@@ -75,12 +75,12 @@ int main()
     glEnable(GL_DEPTH_TEST);
     Entity* entity = world._EntityManager->AddEntity();
     entity->_Mesh = new Mesh(sizeof(vertices), vertices);
-    entity->_Shader = new Shader("fragment.vs", "vertex.fs");
+    entity->_Shader = new Shader("vertex.vs", "fragment.fs");
     entity->_Texture = new Texture("uv-test.jpg");
     entity->_Translation = new Translation(glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0)));
     Entity* entity2 = world._EntityManager->AddEntity();
     entity2->_Mesh = new Mesh(sizeof(vertices), vertices);
-    entity2->_Shader = new Shader("6.1.coordinate_systems.vs", "6.1.coordinate_systems.fs");
+    entity2->_Shader = new Shader("vertex.vs", "fragment.fs");
     entity2->_Texture = new Texture("cat.jpg");
     entity2->_Translation = new Translation(glm::translate(glm::mat4(1.0f), glm::vec3(2, 0, 0)));
     std::cout << entity->_Shader->ID << " and " << entity2->_Shader->ID << std::endl;
